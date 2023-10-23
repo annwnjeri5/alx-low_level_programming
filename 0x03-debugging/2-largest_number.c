1e14cfba08e60d6f121d261c28de909fe7ed7f9d
annwnjeri5 <annwnjeri5@gmail.com>
2023-10-23 02:40:17 -0700

    task 2 debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#include "holberton.h"
->>>>>>> 8e0b627e1622f8c65343462e8f4e45896dae70f3
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -11,7 +7,7 @@
  * @c: third integer
  * Return: largest number
  */
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 {
 	int largest;
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,20 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-<<<<<<< HEAD
 
 void positive_or_negative(int i);
-int main(void)
-int largest_number(int a, int b, int c)
-int convert_day(int month, int day)
-void print_remaining_days(int month, int day, int year)
-#endif 
-=======
-int_putchar(char c);
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
-int convert_day(int month, int day)
-void print_remaining_days(int month, int day, int year)
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-#endif
->>>>>>> 8e0b627e1622f8c65343462e8f4e45896dae70f3
+#endif /* MAIN_/H */

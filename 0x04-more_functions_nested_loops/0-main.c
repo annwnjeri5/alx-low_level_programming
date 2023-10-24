bfb03b8343d4e02a216d27064784bc7a54a87579
annwnjeri5 <annwnjeri5.com>
2023-10-24 08:24:57 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -6,16 +6,13 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
 	char c;
 
 	c = 'A';
-	printf("%c: %d\n", c, _isupper(
+	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isupper(c));
 	return (0);
-
 }
